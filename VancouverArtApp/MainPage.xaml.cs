4df78da509970e7090f5a1b6e4fbcebde68fc8ae
SomeGuyNamedDave <kayaktohell@hotmail.ca>
2015-05-16 17:46:03 -0400

    added map

--- VancouverArtApp/MainPage.xaml.cs
+++ VancouverArtApp/MainPage.xaml.cs
@@ -5,6 +5,7 @@ using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using System.Threading.Tasks;
+using Windows.Devices.Geolocation;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -34,14 +35,16 @@ namespace VancouverArtApp
             this.InitializeComponent();
 
             AddArtItemsData();
+
+   
         }
 
         private async void AddArtItemsData()
         {
-            var item1 = new art_items { Title = "The Drop", Artist= "Inges Idee", Latitude= 49.289426, Longitude=-123.114158, ImageUrl="", Tags="sculpture", Description= "The 20m tall piece is covered with Styrofoam and blue polyurethane. The sculpture is an homage to the power of nature and represents the relationship and outlook towards the water that surrounds us." };
+            var item1 = new art_items { Title = "The Drop", Artist= "Inges Idee", Latitude= 49.289426, Longitude=-123.114158, ImageUrl= "http://1drv.ms/1d7VwT7", Tags="sculpture", Description= "The 20m tall piece is covered with Styrofoam and blue polyurethane. The sculpture is an homage to the power of nature and represents the relationship and outlook towards the water that surrounds us." };
             await artItemsTable.InsertAsync(item1);
 
-            var item2 = new art_items { Title = "Digital Orca", Artist = "Douglas Coupland", Latitude = 49.289798, Longitude = -123.116801, ImageUrl = "", Tags = "sculpture", Description = "Made using a steel armature with aluminum cladding, black and white cubes comprise the orca’s body, making a familiar symbol of the West Coast become something unexpected and new." };
+            var item2 = new art_items { Title = "Digital Orca", Artist = "Douglas Coupland", Latitude = 49.289798, Longitude = -123.116801, ImageUrl = "http://1drv.ms/1L9LE6o", Tags = "sculpture", Description = "Made using a steel armature with aluminum cladding, black and white cubes comprise the orca’s body, making a familiar symbol of the West Coast become something unexpected and new." };
             await artItemsTable.InsertAsync(item2);
 
             var item3 = new art_items { Title = "Light Shed", Artist = "Liz Magor", Latitude = 49.291371, Longitude = -123.123373, ImageUrl = "", Tags = "sculpture", Description = "Constructed mostly of aluminum, it is based on the shape of an old boat shed and at night it glows softly from the inside." };
