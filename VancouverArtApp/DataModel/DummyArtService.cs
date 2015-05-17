dcbd3ef864abe0b81e6fa39877554a13d6c4e7c8
Jan Hannemann <jan.hannemann@outlook.com>
2015-05-16 19:45:47 -0700

    list view designed

--- VancouverArtApp/DataModel/DummyArtService.cs
+++ VancouverArtApp/DataModel/DummyArtService.cs
@@ -13,10 +13,10 @@ namespace VancouverArtApp.DataModel
             await Task.Delay(0);
             var items = new List<art_items>();
 
-            items.Add(new art_items() { Title = "Awesome picture", Artist = "Jan" });
-            items.Add(new art_items() { Title = "Me", Artist = "Jan" });
-            items.Add(new art_items() { Title = "Awesome picture", Artist = "Jan" });
-            items.Add(new art_items() { Title = "Awesome picture", Artist = "Jan" });
+            items.Add(new art_items() { Title = "Awesome picture", Artist = "Jan", Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent feugiat dui ut accumsan consectetur. Fusce at ipsum dui. Suspendisse varius quam sit amet eros suscipit, in vehicula massa vestibulum. Suspendisse potenti. Nam feugiat urna et ante pretium iaculis. Quisque mattis velit ex, eu interdum leo maximus a. Nunc tempus sem facilisis egestas pulvinar. Pellentesque efficitur elit viverra, tristique ex sit amet, eleifend risus." });
+            items.Add(new art_items() { Title = "Me", Artist = "Jan", Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent feugiat dui ut accumsan consectetur. Fusce at ipsum dui. Suspendisse varius quam sit amet eros suscipit, in vehicula massa vestibulum. Suspendisse potenti. Nam feugiat urna et ante pretium iaculis. Quisque mattis velit ex, eu interdum leo maximus a. Nunc tempus sem facilisis egestas pulvinar. Pellentesque efficitur elit viverra, tristique ex sit amet, eleifend risus." });
+            items.Add(new art_items() { Title = "Awesome picture", Artist = "Jan", Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent feugiat dui ut accumsan consectetur. Fusce at ipsum dui. Suspendisse varius quam sit amet eros suscipit, in vehicula massa vestibulum. Suspendisse potenti. Nam feugiat urna et ante pretium iaculis. Quisque mattis velit ex, eu interdum leo maximus a. Nunc tempus sem facilisis egestas pulvinar. Pellentesque efficitur elit viverra, tristique ex sit amet, eleifend risus." });
+            items.Add(new art_items() { Title = "Awesome picture", Artist = "Jan", Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent feugiat dui ut accumsan consectetur. Fusce at ipsum dui. Suspendisse varius quam sit amet eros suscipit, in vehicula massa vestibulum. Suspendisse potenti. Nam feugiat urna et ante pretium iaculis. Quisque mattis velit ex, eu interdum leo maximus a. Nunc tempus sem facilisis egestas pulvinar. Pellentesque efficitur elit viverra, tristique ex sit amet, eleifend risus." });
             return items;
         }
     }
